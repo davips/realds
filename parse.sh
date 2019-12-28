55929bae3bd66191859cf701370d30cd5b9d2988
davips <dpsabc@gmail.com>
2019-12-27 23:40:14 -0300

    parse run-log.sh

---
+++ parse.sh
@@ -0,0 +1,1 @@
+scala -nc parser.scala
--- parser.scala
+++ parser.scala
@@ -2,7 +2,7 @@
 // 	val arr = l.split(", ")
 // 	return if (arr.last != "Infinity") (arr.dropRight(1) :+ "hit").mkString(", ") else l}
 
-val lines = io.Source.fromFile("output.txt").getLines.filter(!_.startsWith("]")).map(_.replace("[ \"", "").replace(", \"", "").replace("\"", "")).map(_ + "\n")
+val lines = io.Source.fromFile("output.log").getLines.filter(!_.startsWith("]")).map(_.replace("[ \"", "").replace(", \"", "").replace("\"", "")).map(_ + "\n")
 
 val atts = lines.take(1).toList(0).split(", ").dropRight(1).zipWithIndex.map{case (_, i) => s"@attribute a$i numeric"}.mkString("\n")
 
